fd21a83b38ee2aa5bdde7eefe439105a684eb2c9
Kiel Arneson <kiel.arneson@gmail.com>
2021-10-21 18:49:09 -0500

    Added associations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+  has_secure_password
+  validates :email, presence: true, uniqueness: true
+
+  has_many :tailgates
+  has_many :games, through: :tailgates
+  has_many :lodgings
+  has_many :parkings
 end
