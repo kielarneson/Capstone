4e6306250adc5dcca45aa4bb62a189cf1c218d8b
Kiel Arneson <kiel.arneson@gmail.com>
2021-10-21 19:05:15 -0500

    Modified seeds file by adding games in order to get rails to seed database properly

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,6 +3,9 @@ User.create!(first_name: "Peter", last_name: "Jang", user_name: "peterdude", ema
 User.create!(first_name: "Blake", last_name: "Sims", user_name: "blakedude", email: "blake@test.com", password: "password", password_confirmation: "password")
 User.create!(first_name: "Jake", last_name: "Coker", user_name: "jakedude", email: "jake@test.com", password: "password", password_confirmation: "password")
 
+Game.create!(api_id: 2, name: "Cincinnati at Notre Dame", away_team: "Cincinnati", away_team_conference: "American", home_team: "Notre Dame", home_team_conference: "Independent", stadium: "Notre Dame Stadium", address: "2010 Moose Krause Cir, Notre Dame, IN 46556", start_time: "2021-10-03T13:30:00-6:00")
+Game.create!(api_id: 3, name: "Ole Miss at Alabama", away_team: "Ole Miss", away_team_conference: "SEC", home_team: "Alabama", home_team_conference: "SEC", stadium: "Bryant-Denny Stadium", address: "920 Paul W Bryant Dr, Tuscaloosa, AL 35401", start_time: "2021-10-03T14:30:00-6:00")
+
 Tailgate.create!(user_id: 1, game_id: 2, name: "Arneson Family Tailgate", description: "Many words...", address: "731 12th St, Tuscaloosa, AL 35401", start_time: "2021-10-03T09:00:00-6:00", end_time: "2021-10-03T13:00:00-6:00")
 Tailgate.create!(user_id: 2, game_id: 1, name: "Peter Party", description: "Many words...", address: "100, Eck Visitors Center, Notre Dame, IN 46556", start_time: "2021-10-03T11:00:00-6:00", end_time: "2021-10-03T13:00:00-6:00")
 
