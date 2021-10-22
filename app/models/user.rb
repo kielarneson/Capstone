394f87179d6627fe4cb461830d0b860dffa51b86
Kiel Arneson <kiel.arneson@gmail.com>
2021-10-21 22:14:06 -0500

    Changed some user associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   validates :email, presence: true, uniqueness: true
 
   has_many :tailgate_users
-  has_many :tailgates, through: :tailgate_users
+  has_many :tailgates
   has_many :games, through: :tailgates
   has_many :lodgings
   has_many :parkings
