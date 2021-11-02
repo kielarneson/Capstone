289ec52f0df948b1ea7c5959472229c5817c3d77
Kiel Arneson <kiel.arneson@gmail.com>
2021-11-02 12:23:09 -0500

    Updated seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,10 +9,11 @@ Game.create!(api_id: 3, name: "Ole Miss at Alabama", away_team: "Ole Miss", away
 Tailgate.create!(user_id: 1, game_id: 2, name: "Arneson Family Tailgate", description: "Many words...", address: "731 12th St, Tuscaloosa, AL 35401", start_time: "2021-10-03T09:00:00-6:00", end_time: "2021-10-03T13:00:00-6:00")
 Tailgate.create!(user_id: 2, game_id: 1, name: "Peter Party", description: "Many words...", address: "100, Eck Visitors Center, Notre Dame, IN 46556", start_time: "2021-10-03T11:00:00-6:00", end_time: "2021-10-03T13:00:00-6:00")
 
-TailgateUser.create!(user_id: 2, tailgate_id: 2, requested: true, accepted: true, review: "Peter was awesome!", overall_rating: 7.8, truthful_rating: 10, location_rating: 8, hospitality_rating: 8, amenities_rating: 7, food_rating: 6, drink_rating: 4)
-TailgateUser.create!(user_id: 4, tailgate_id: 1, requested: true, accepted: false)
+TailgateUser.create!(user_id: 2, tailgate_id: 2, game_id: 1, requested: true, accepted: true, review: "Peter was awesome!", overall_rating: 7.8, truthful_rating: 10, location_rating: 8, hospitality_rating: 8, amenities_rating: 7, food_rating: 6, drink_rating: 4)
+TailgateUser.create!(user_id: 4, tailgate_id: 1, game_id: 2, requested: true, accepted: false)
 
 Lodging.create!(user_id: 3, tailgate_id: 1, lodging_type: "Airbnb", lodging_name: "Paul's Northport house", address: "2015 McFarland Blvd, Northport, AL 35476")
-Lodging.create!(user_id: 4, tailgate_id: 2, lodging_type: "hotel", lodging_name: "Marriot", address: "215 W Day Rd, Mishawaka, IN 46545")
+Lodging.create!(user_id: 4, tailgate_id: 1, lodging_type: "hotel", lodging_name: "Marriot", address: "215 W Day Rd, Mishawaka, IN 46545")
 
 Parking.create!(user_id: 3, tailgate_id: 1, parking_type: "street", address: "1201 Coliseum Dr, Tuscaloosa, AL 35401")
+Parking.create!(user_id: 4, tailgate_id: 1, parking_type: "garage", address: "345 N 3rd St, Tuscaloosa, AL 35401")
