a319fa6703e184413f248c59d389b620382a97c5
Kiel Arneson <kiel.arneson@gmail.com>
2021-11-09 20:54:26 -0600

    Fixed strftime conversion in tailgate model to display time conversions AM and PM properly

--- app/models/tailgate.rb
+++ app/models/tailgate.rb
@@ -4,18 +4,18 @@ class Tailgate < ApplicationRecord
   has_many :tailgate_users
 
   def start_time_conversion
-    if start_time.strftime("%I:%MPM")[0] == "0"
-      start_time.strftime("%I:%MPM").slice(1..)
+    if start_time.strftime("%I:%M%p")[0] == "0"
+      start_time.strftime("%I:%M%p").slice(1..)
     else
-      start_time.strftime("%I:%MPM")
+      start_time.strftime("%I:%M%p")
     end
   end
 
   def end_time_conversion
-    if end_time.strftime("%I:%MPM - %A, %B %e, %Y")[0] == "0"
-      end_time.strftime("%I:%MPM - %A, %B %e, %Y").slice(1..)
+    if end_time.strftime("%I:%M%p - %A, %B %e, %Y")[0] == "0"
+      end_time.strftime("%I:%M%p - %A, %B %e, %Y").slice(1..)
     else
-      end_time.strftime("%I:%MPM - %A, %B %e, %Y")
+      end_time.strftime("%I:%M%p - %A, %B %e, %Y")
     end
   end
 end
